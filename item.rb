1f072d28e8b5a6f71f4b1372cdcd7d4b7138a293
Anastasiia Demian <nastyha399@gmail.com>
2022-12-21 16:35:45 +0200

    Create application skeleton with gemfile

---
+++ item.rb
@@ -0,0 +1,17 @@
+#class Item (item.rb), реалізувавши конструктор initialize; метод info (який може приймати на вході блок)
+#для видачі інформації про продукт у форматі, який диктує блок; to_s (для коректної роботи метода :puts)
+#та to_h (формування хешу на базі даних та назв ключів). Використати необхідні attr_accessor для атрибутів (яких мінімум має бути 5).
+class Item
+  def initialize()
+    #TODO
+  end
+  def info(format)
+    #TODO
+  end
+  def to_s()
+    #TODO
+  end
+  def to_h()
+    #TODO
+  end
+end
